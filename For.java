f9b1842d09b6d1a12236f32bfbc198d07f356a98
Jirattanun <49854272+Tien-Jirattanun@users.noreply.github.com>
2020-04-10 00:47:52 +0700

    Update For.java

--- For.java
+++ For.java
@@ -10,15 +10,18 @@ public class For
         }
         System.out.println();
         System.out.println("------------------------------------");
+        //simple for
         for (int i = 0;i<10;i += 2)
         {
             System.out.print(i);
         }
         System.out.println();
         System.out.println("------------------------------------");
+        //advance for
         String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
         for (String i : cars) {
             System.out.println(i);
+        //array+for
         }
     }
 }
