73fdd8d22fa65ee3836beb47bc866ad98abc73f9
Jirattanun <49854272+Tien-Jirattanun@users.noreply.github.com>
2020-04-12 17:50:49 +0700

    Update Switch.java

--- Switch.java
+++ Switch.java
@@ -7,14 +7,14 @@ public class Switch
         int day = 4;
         switch (day)
         {
-            case 6 :
+            case 6 :                                        //if day == 6 do case 6
                 System.out.println("Today is Saturday");
                 break;
-            case 7 :
-                System.out.println("Today is Sunday");
+            case 7 :                                        //if day == 7 do case 7
+                System.out.println("Today is Sunday");      
                 break;
             default :
-                System.out.println("Looking forward to the Weekend");
+                System.out.println("Looking forward to the Weekend");     //else
         }
     }
 }
