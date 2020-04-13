fca451d34ad437e25c5a2131d6712b4acdb7a628
Jirattanun <49854272+Tien-Jirattanun@users.noreply.github.com>
2020-04-13 17:13:25 +0700

    Update math.java

--- math.java
+++ math.java
@@ -4,10 +4,10 @@ public class math
 {
     public static void main(String[] args)
     {
-        Math.max(3, 6);
-        Math.min(5, 10);
-        Math.sqrt(5);
-        Math.abs(-14);
-        Math.random();
+        Math.max(3, 6);                 //find max
+        Math.min(5, 10);                //find min
+        Math.sqrt(5);                   //find square root
+        Math.abs(-14);                  //absolute value
+        Math.random();                  //random
     }
 }
