1082e45b62993c13fe5b6024c88dec056c5c2f2d
Jirattanun <49854272+Tien-Jirattanun@users.noreply.github.com>
2020-04-13 17:06:25 +0700

    Update While.java

--- While.java
+++ While.java
@@ -5,7 +5,7 @@ public class While
     public static void main(String[] args)
     {
        int i = 0;
-       while (i<5)
+       while (i<5)                                      //while the statement is true do it
        {
            System.out.print(i);
            System.out.print(" ");
@@ -14,7 +14,7 @@ public class While
        System.out.println();
        System.out.println("----------------------------------");
        i = 0;
-       do
+       do                                               //do first and check statement
        {
            System.out.print(i);
            System.out.print(" ");
