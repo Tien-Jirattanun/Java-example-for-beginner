b1b9577d32ccc86badcb15c18a22fe68442cfc30
Jirattanun <49854272+Tien-Jirattanun@users.noreply.github.com>
2020-04-13 17:23:54 +0700

    Update string.java

--- string.java
+++ string.java
@@ -7,17 +7,17 @@ public class string
         String h = "the world";
         String firstName = "John";
         String lastName = "Doe";
-        System.out.println(firstName + " " + lastName);
-        System.out.println(h.length());
-        System.out.println(h.toUpperCase());
-        System.out.println(h.toLowerCase());
-        System.out.println(h.indexOf("world"));
-        System.out.println(firstName.concat(lastName));
-        System.out.println("jirattanun\nleeudomwong");
-        System.out.println("jirattanun\rleeudonwong");
-        System.out.println("jirattanun\tleeudonwong");
-        System.out.println("jirattanun\bleeudomwong");
-        System.out.println("jirattanun\fleeudomwong");
+        System.out.println(firstName + " " + lastName);       //jhon Dep
+        System.out.println(h.length());                       //9
+        System.out.println(h.toUpperCase());                  //uppercase
+        System.out.println(h.toLowerCase());                  //lower case
+        System.out.println(h.indexOf("world"));               //5
+        System.out.println(firstName.concat(lastName));       //firstname + lastname
+        System.out.println("jirattanun\nleeudomwong");        //new line
+        System.out.println("jirattanun\rleeudonwong");        //carriage return
+        System.out.println("jirattanun\tleeudonwong");        //horizontal tab
+        System.out.println("jirattanun\bleeudomwong");        //backspace
+        System.out.println("jirattanun\fleeudomwong");        //form feed
 
     }
 }
